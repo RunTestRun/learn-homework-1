d5d0a1b9ab64062110402a876bdb41f3351e755f
Artem Kudelin <shadow347@mail.ru>
2021-03-04 01:27:54 +0300

    Выполнено задание на применение цикла for

--- 3_for.py
+++ 3_for.py
@@ -11,11 +11,33 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+
+    school = [
+        {'school_class': '1a', 'scores': [5,3,5,2,5]}, 
+        {'school_class': '2a', 'scores': [5,2,1,4,3]}, 
+        {'school_class': '3a', 'scores': [2,4,3,5,5]},
+        {'school_class': '4a', 'scores': [5,4,5,4,5]},
+        {'school_class': '5a', 'scores': [1,2,3,4,5]}
+    ]
+
+    scores_sum = 0
+    scores_len = 0
+    num = 0
+
+    for score in school:
+        scores_sum += sum(school[num]['scores'])
+        scores_len += len(school[num]['scores'])
+        num += 1
+
+    print(f"Средний балл по всей школе: {scores_sum/scores_len}")
+
+    scores_sum = 0
+    num = 0
+
+    for score in school:
+        scores_sum = sum(school[num]['scores'])/len(school[num]['scores'])
+        print(f"Средний балл по классу {school[num]['school_class']} составляет: {scores_sum}")
+        num += 1
+
 if __name__ == "__main__":
     main()
